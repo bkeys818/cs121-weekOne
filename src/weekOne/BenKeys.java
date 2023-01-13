a9f6e5087c0ad6f925bfcaf829cf92cd81408f98
Ben Keys <bkeys818@gmail.com>
2023-01-13 13:19:03 -0500

    Initial commit

---
+++ src/weekOne/BenKeys.java
@@ -0,0 +1,20 @@
+package weekOne;
+
+public class BenKeys {
+
+    public static void main(String[] args) {
+        // declare variables
+        int age;
+        double gpa;
+        char letterGrade;
+        boolean csMajor;
+        String name;
+
+        // initialize variables
+        age = 21;
+        gpa = 2.9;
+        letterGrade = 'B';
+        csMajor = false;
+        name = "Ben Keys";
+    }
+}
